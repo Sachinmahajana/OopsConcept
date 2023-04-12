596bcd139b3df8994092c20c24c206cce6add51a
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-12 14:04:20 +0530

    [Sachin] Adding Singleinheritance

--- OopsConcept/Program.cs
+++ OopsConcept/Program.cs
@@ -2,9 +2,18 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+       public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Oops Concept");
+
+            // Sinle inheritance
+            Mghector myCar = new Mghector();
+            myCar.model = "Turbo hector 1.5";
+            myCar.colour = "White";
+            myCar.price = 1400000;
+            myCar.mileage = 13.79f;
+            myCar.PrintCarDetails();
+            myCar.PrintMileage();
         }
     }
 }
---
+++ OopsConcept/SingleInheritance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public class Car
+    {
+        public string model;
+        public string colour;
+        public long price;
+        public void PrintCarDetails()
+        {
+            Console.WriteLine("Car model is: " + model);
+            Console.WriteLine("Car colour is:" + colour);
+            Console.WriteLine("Car price is:" + price);
+        }
+    }
+    public class Mghector:Car
+    {
+        public float mileage;
+        public void PrintMileage()
+        {
+            Console.WriteLine("Car mileage is:" + mileage);
+        }
+
+    }
+}
